fc54eae62ef1791c65b2d377a81490913c520e95
stefcho034 <s.lazarov@outlook.com>
2020-02-24 14:56:40 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue; 
+
+namespace QueueApp
+{
+
+   
+ class Program
+
+
+    {
+
+       private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=stefcho034;AccountKey=Hmkja0Zu2u+OA8Fz7iL7vrX5BkjfXsqISS+Db6B3LS0vTPY51vxrIJ0NF6895csi8YOss5XiOD31paWVYvV2+A==";
+       
+
+        static CloudQueue GetQueue()
+{
+    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+    CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+    return queueClient.GetQueueReference("newsqueue");
+}
+
+       static async Task SendArticleAsync(string newsMessage)
+{
+    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+    CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+    CloudQueue queue = queueClient.GetQueueReference("newsqueue");
+    bool createdQueue = await queue.CreateIfNotExistsAsync();
+    if (createdQueue)
+    {
+        Console.WriteLine("The queue of news articles was created.");
+    }
+
+    CloudQueueMessage articleMessage = new CloudQueueMessage(newsMessage);
+    await queue.AddMessageAsync(articleMessage);
+}
+
+
+
+static async Task<string> ReceiveArticleAsync()
+{
+    CloudQueue queue = GetQueue();
+    bool exists = await queue.ExistsAsync();
+    if (exists)
+    {
+        CloudQueueMessage retrievedArticle = await queue.GetMessageAsync();
+        if (retrievedArticle != null)
+        {
+            string newsMessage = retrievedArticle.AsString;
+            await queue.DeleteMessageAsync(retrievedArticle);
+            return newsMessage;
+        }
+    }
+
+    return "<queue empty or not created>";
+}
+
+
+
+
+
+static async Task Main(string[] args)
+{
+    if (args.Length > 0)
+    {
+        string value = String.Join(" ", args);
+        await SendArticleAsync(value);
+        Console.WriteLine($"Sent: {value}");
+    }
+
+else
+{
+    string value = await ReceiveArticleAsync();
+    Console.WriteLine($"Received {value}");
+}
+
+}
+
+    }
+}
